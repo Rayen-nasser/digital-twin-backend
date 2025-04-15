ecb1f1539aba54726641c5c9b141541049c7bba7
rayen <rayenn38@gmail.com>
2025-04-15 10:20:39 +0100

    implment urls Configuration

---
+++ chatbot/user/urls.py
@@ -0,0 +1,26 @@
+# core/urls.py
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import (
+    RegisterView,
+    LoginView,
+    UserProfileView,
+    EmailVerificationView,
+    ChangePasswordView,
+    PasswordResetRequestView,
+    PasswordResetConfirmView,
+    LogoutView
+)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
+    path('verify-email/', EmailVerificationView.as_view(), name='verify_email'),
+    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
+    path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
