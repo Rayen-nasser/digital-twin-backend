ef6861b374de5865399a60cd1ec2a5138fe98200
rayen <rayenn38@gmail.com>
2025-04-15 14:49:40 +0100

    implment swagger package(API doc)

--- chatbot/chatbot/urls.py
+++ chatbot/chatbot/urls.py
@@ -1,8 +1,33 @@
 # chatbot/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Authentication API",
+        default_version='v1',
+        description="Authentication APIs for your application",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('user.urls')),
+
+    # Swagger UI
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
