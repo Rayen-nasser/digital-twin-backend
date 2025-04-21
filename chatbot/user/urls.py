c5e467bb14d7cc758e5052a654028f76e8ebf5b4
rayen <rayenn38@gmail.com>
2025-04-21 18:21:16 +0100

    Update user URLs to include email update, delete profile image endpoint

--- chatbot/user/urls.py
+++ chatbot/user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import (
+    ProfileImageView,
     RegisterView,
     LoginView,
     UserProfileView,
@@ -18,6 +19,7 @@ urlpatterns = [
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 
     path('profile/', UserProfileView.as_view(), name='user_profile'),
+    path('profile/image/', ProfileImageView.as_view(), name='profile-image'),
     path('verify-email/', EmailVerificationView.as_view(), name='verify_email'),
     path('change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
