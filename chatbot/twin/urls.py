48d4c4c55554e8bcf01c30d96beeb3a30540fd4b
rayen <111684397+Rayen-nasser@users.noreply.github.com>
2025-05-06 09:51:50 +0100

    Add route for SharedWithMeViewSet to handle shared twins

--- chatbot/twin/urls.py
+++ chatbot/twin/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TwinViewSet
+from .views import SharedWithMeViewSet, TwinViewSet
 
 router = DefaultRouter()
 router.register(r'', TwinViewSet, basename='twin')
+router.register(r'shared-twins', SharedWithMeViewSet, basename='shared-twin')
 
 urlpatterns = [
     path('', include(router.urls)),
