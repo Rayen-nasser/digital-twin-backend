0e8e212c8852aad90523e9441ced2c7f4ba4bb85
rayen nasser <rayenn38@gmail.com>
2025-05-23 09:33:47 +0100

    Register HeyGenStreamingViewSet in the URL routing for streaming operations

--- chatbot/messaging/urls.py
+++ chatbot/messaging/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
 from messaging import consumers
-from .views import MessageViewSet, UserTwinChatViewSet, VoiceRecordingViewSet
+from .views import HeyGenStreamingViewSet, MessageViewSet, UserTwinChatViewSet, VoiceRecordingViewSet
 
 router = DefaultRouter()
 router.register(r'chats', UserTwinChatViewSet, basename='chat')
 router.register(r'messages', MessageViewSet, basename='message')
 router.register(r'voice-recordings', VoiceRecordingViewSet, basename='voice-recording'),
+router.register(r'heygen-streaming', HeyGenStreamingViewSet, basename='heygen-streaming')
 
 urlpatterns = [
     path('', include(router.urls)),
