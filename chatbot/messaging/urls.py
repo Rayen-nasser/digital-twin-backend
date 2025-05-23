1d9e58e24c6c14f61cbab39f4d11f4f4f3aa6d21
rayen nasser <rayenn38@gmail.com>
2025-05-23 09:36:52 +0100

    Remove HeyGenStreamingViewSet from URL routing and views

--- chatbot/messaging/urls.py
+++ chatbot/messaging/urls.py
@@ -2,13 +2,12 @@ from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
 from messaging import consumers
-from .views import HeyGenStreamingViewSet, MessageViewSet, UserTwinChatViewSet, VoiceRecordingViewSet
+from .views import git addMessageViewSet, UserTwinChatViewSet, VoiceRecordingViewSet
 
 router = DefaultRouter()
 router.register(r'chats', UserTwinChatViewSet, basename='chat')
 router.register(r'messages', MessageViewSet, basename='message')
-router.register(r'voice-recordings', VoiceRecordingViewSet, basename='voice-recording'),
-router.register(r'heygen-streaming', HeyGenStreamingViewSet, basename='heygen-streaming')
+router.register(r'voice-recordings', VoiceRecordingViewSet, basename='voice-recording')
 
 urlpatterns = [
     path('', include(router.urls)),
